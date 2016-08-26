f3c64a33f4485232d7be776150f0fd7e2ee555d1
Brian Mont <briansmont830@gmail.com>
2016-08-26 12:40:30 +0000

    Changed code so the first argument is the greeting

--- code/greeting.rb
+++ code/greeting.rb
@@ -1,6 +1,10 @@
 def greeting
-    ARGV.each do |arg|
-        puts "Hey #{arg}"
+    greeting = ARGV[0]
+    ARGV.each_with_index do |val, index|
+        if index != 0
+            name = val
+            puts "#{greeting}, #{name}!"
+        end
     end
 end
 
