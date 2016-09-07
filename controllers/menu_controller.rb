91d9b38ef41c350d5d372bf31f2a2afa45bb7b7e
Brian Mont <briansmont830@gmail.com>
2016-09-07 16:22:28 +0000

    Ruby Assignment 6 - entry by number and submenu, small edits based on video solution for organization

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - View Entry Number n "
+     puts "6 - Exit"
      print "Enter your selection: "
      
      selection = gets.to_i
@@ -37,6 +38,10 @@ class MenuController
             read_csv
             main_menu
          when 5
+            system "clear"
+            entry_n_submenu
+            main_menu
+         when 6
             puts "Good-bye!"
             exit(0)
          else
@@ -99,6 +104,20 @@ class MenuController
             puts "#{selection} is not a valid input"
             entry_submenu(entry)
        end
+    end
+    def entry_n_submenu
+        print "Entry number to view: "
+        selection = gets.chomp.to_i
+        
+        if selection < @address_book.entries.count
+            puts @address_book.entries[selection]
+            puts "Press enter to return to main menu"
+            gets.chomp
+            system "clear"
+        else
+            puts "#{selection} isn't valid input"
+            entry_n_submenu
+        end
     end
     
     
